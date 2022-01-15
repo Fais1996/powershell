3f4fdf8b616ba226ff6454312e0b0faea75a4505
muhammed faisal <97455685+Fais1996@users.noreply.github.com>
2022-01-15 22:55:46 +0530

    Add files via upload

---
+++ scripttorebootodd.ps1
@@ -0,0 +1,11 @@
+﻿$servers= Get-Content c:\temp\servers.txt
+$number=0
+while ($number -lt 21)
+{
+$server1 = $number 
+$server2 = $number + 1
+Restart-Computer -ComputerName $servers[$server1], $servers[$server2] -Force
+write-host "rebootingservers" $servers[$server1], $servers[$server2]
+Start-Sleep -Seconds 180
+$number = $number +2
+}
